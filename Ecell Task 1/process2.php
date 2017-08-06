15272a3585fe2477165c682ec9434f6a132860fd
Pranav4399 <psriram37@gmail.com>
2017-08-06 22:56:32 +0530

    Update process2.php

--- Ecell Task 1/process2.php
+++ Ecell Task 1/process2.php
@@ -1,7 +1,4 @@
 <?php
-
-
-session_start();
 $db=mysqli_connect("localhost","root","","register");
 if (isset($_POST['submit'])) {
    
@@ -9,26 +6,17 @@ if (isset($_POST['submit'])) {
 	$RollNo=mysql_real_escape_string($_POST['PhoneNo']);
 	$email=mysql_real_escape_string($_POST['email']);
 	$department=mysql_real_escape_string($_POST['department']);
-	$pass=mysql_real_escape_string($_POST['pass']);
-	$confirmpass=mysql_real_escape_string($_POST['confirmpass']);
-    $year=mysql_real_escape_string($_POST['department']);
+	$year=mysql_real_escape_string($_POST['year']);
     $resume=mysql_real_escape_string($_POST['resume']);
    
-	if ($pass == $confirmpass) 
-	{
-		$pass=md5($pass);
-		$sql="INSERT INTO userss(Name,PhoneNo,email,department,pass,year,resume) VALUES('$Name','$RollNo','$email','$department','$pass','$year','$resume')";
+		$sql="INSERT INTO userss(Name,PhoneNo,email,department,year,resume) VALUES('$Name','$RollNo','$email','$department','$year','$resume')";
 		mysqli_query($db, $sql);
 		echo "Sign up success";
 		
 		
-	}
-	else
-	{
-		echo "passwords dont match";
-       $_SESSION['message']="The two passwords do not match";
+	
+	   
 
-	}
 }
 ?>
 
